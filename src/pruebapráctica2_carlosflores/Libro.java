7cc77970781b165518a6822d4ab845324b492d3a
Daniel-jv <carlosfloreseagle2020@gmail.com>
2024-03-09 10:48:48 -0600

    avance 1

--- src/pruebapráctica2_carlosflores/Libro.java
+++ src/pruebapráctica2_carlosflores/Libro.java
@@ -1,13 +1,84 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package pruebapráctica2_carlosflores;
 
-/**
- *
- * @author carlo
- */
+
 public class Libro {
+    
+    public String Nombre;
+    public String Autor;
+    public int ID;
+    public String estado;
+    public String dueño;
+
+    public Libro(String Nombre, String Autor, int ID, String estado, String dueño) {
+        this.Nombre = Nombre;
+        this.Autor = Autor;
+        this.ID = ID;
+        this.estado = estado;
+        this.dueño = dueño;
+    }
+
+    public static void prestar(Libro libros [], int pos, String name){
+        if(libros[pos].getEstado().contentEquals("disponible")){
+            libros[pos].setEstado("prestado");
+            libros[pos].setDueño(name);
+        }else{
+            System.out.println("El libro no esta disponible, esta prestado");
+        }
+    }
+    
+    public static void regresar(Libro libros [], int pos){
+        if(libros[pos].getEstado().contentEquals("prestado")){
+            libros[pos].setEstado("disponible");
+            libros[pos].setDueño("");
+        }else{
+            System.out.println("El libro no esta prestado, esta disponible");
+        }
+    }
+    
+    public String toString(){
+        return "Nombre: " + Nombre + "Autor: " + Autor + ", ID: " + ID + "Estado: " + estado + "Dueño: "+ dueño;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getAutor() {
+        return Autor;
+    }
+
+    public void setAutor(String Autor) {
+        this.Autor = Autor;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getDueño() {
+        return dueño;
+    }
+
+    public void setDueño(String dueño) {
+        this.dueño = dueño;
+    }
+    
+    
     
 }
--- src/pruebapráctica2_carlosflores/PruebaPráctica2_CarlosFlores.java
+++ src/pruebapráctica2_carlosflores/PruebaPráctica2_CarlosFlores.java
@@ -1,20 +1,93 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package pruebapráctica2_carlosflores;
 
-/**
- *
- * @author carlo
- */
+import java.util.Scanner;
+import static pruebapráctica2_carlosflores.Libro.prestar;
+import static pruebapráctica2_carlosflores.Libro.regresar;
+
 public class PruebaPráctica2_CarlosFlores {
 
-    /**
-     * @param args the command line arguments
-     */
+    static Scanner leer = new Scanner(System.in);
+
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        Libro libreria [] = new Libro [0];
+        boolean var = true;
+        int op = 0;
+        do{
+            System.out.println("1) Agregar Nuevo libro a la biblioteca\n2) Solicitar libro\n3) Regresar libro\n4) Buscar libro por nombre\n5) Listar libros disponibles\n6) Listar libros prestados\n7) Salir");
+            op = leer.nextInt();
+            switch(op){
+                
+                case 1:{
+                    break;
+                }//fin case 1
+                
+                case 2:{
+                    String name = leer.next();
+                    int pos = leer.nextInt();
+                    prestar(libreria,pos,name);
+                    break;
+                }//fin case 2
+                
+                case 3:{
+                    int pos = leer.nextInt();
+                    regresar(libreria,pos);
+                    break;
+                }//fin case 3
+                
+                case 4:{
+                    String buscar = leer.next();
+                    boolean finded = false;
+                    for (int i = 0; i < libreria.length; i++) {
+                        if(libreria[i].getNombre().contentEquals(buscar)){
+                            System.out.println();
+                            finded = true;
+                        }
+                    }
+                    if(finded == false){
+                        System.out.println("No se encontro el libro");
+                    }
+                    break;
+                }//fin case 4
+                
+                case 5:{
+                    int cont = 1;
+                    System.out.println("Libros disponibles:");
+                    for (int i = 0; i < libreria.length; i++) {
+                        if(libreria[i].getEstado().contentEquals("disponible")){
+                            System.out.println(cont+") "+libreria[i].getNombre());
+                            cont++;
+                        }
+                    }
+                    break;
+                }//fin case 5
+                
+                case 6:{
+                    int cont = 1;
+                    System.out.println("Libros prestado:");
+                    for (int i = 0; i < libreria.length; i++) {
+                        if(libreria[i].getEstado().contentEquals("prestado")){
+                            System.out.println(cont+") "+libreria[i].getNombre());
+                            cont++;
+                        }
+                    }
+                    break;
+                }//fin case 6
+                
+                case 7:{
+                    System.out.println("Ha salido de la libreria, nos vemos");
+                    var = false;
+                    break;
+                }//fin case 7
+                
+                default:{
+                    System.out.println("opcion no valida");
+                    break;
+                }//fin default
+                
+            }//fin switch
+            
+        }while (var);//fin do while
+        
+    }//fin main
     
-}
+}//fin class
