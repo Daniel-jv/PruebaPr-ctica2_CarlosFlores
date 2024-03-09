cd01dc94bb15650915f34ae490a573d70ed4b285
Daniel-jv <carlosfloreseagle2020@gmail.com>
2024-03-09 10:02:50 -0600

    ...

---
+++ src/pruebapráctica2_carlosflores/PruebaPráctica2_CarlosFlores.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pruebapráctica2_carlosflores;
+
+/**
+ *
+ * @author carlo
+ */
+public class PruebaPráctica2_CarlosFlores {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
